adf4258f9928da74b7727dd77dd79597e72c93b2
Subarna Pokharel <subarnapokharel252@gmail.com>
2022-11-11 06:15:02 +0545

    Made-renderArray-to-the-screen-manually

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 //This is GameBoard Module
 let gameBoardObject=(function(){
-    let gameBoardArray=[];
+    let gameBoardArray=["O"];
     return{gameBoardArray};
 })();
 
 //This is PlayerInformation Factory Function
 let playerObjectFunction=(playerName,playerNumber,assignmentXO)=>{
-
+   
+    
     return{
         playerName,playerNumber,assignmentXO
     };
 };
+
+//This is function for rendering array to the SCREEN
+let renderArrayToScreen=(function()
+{
+    let gridBoxes=document.querySelectorAll(".grid-box");
+    gridBoxes[0].textContent=gameBoardObject.gameBoardArray;
+    
+
+    console.log("Show the array as seen inside gameboardobject",gameBoardObject);
+    console.log("Show the nodelist of the gameobject ",gameBoardObject.gameBoardArray);
+
+    return{}
+
+})();
 
 //This is GAME DISPLAY FLOW CONTROLLER Module
 let displayFlowController=(function()
@@ -20,3 +35,4 @@ let displayFlowController=(function()
 
 let player1=playerObjectFunction("Subarna",1,"X");
 let player2=playerObjectFunction("Hetal",2,"O");
+renderArrayToScreen;
